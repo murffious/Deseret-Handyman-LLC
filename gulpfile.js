16aa449ca07f27501b908a600cf85dfdb9dbda7c
murffious <titan.murff@gmail.com>
2018-06-14 14:27:06 -0600

    deploy fixes again

--- gulpfile.js
+++ gulpfile.js
@@ -125,12 +125,14 @@ gulp.task('dev', ['css', 'js', 'browserSync'], function () {
   gulp.watch('./*.html', browserSync.reload);
 });
 
-gulp.task('build', ['default']);
-// gulp.task('serveprod', function () {
-//   connect.server({
-//     root: './dist/index.html',
-//     port: process.env.PORT || 5000, // localhost:5000
-//     livereload: false,
-//     fallback: './dist/index.html'
-//   });
-// });
+
+
+gulp.task('serveprod', function () {
+  connect.server({
+    root: './dist/index.html',
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false,
+    fallback: './dist/index.html'
+  });
+});
+gulp.task('build', ['default', 'serverprod']);
